fc75ade26a07013f221acc0dfd53cf92ad28626e
Will Lutz <lbzinuse@gmail.com>
2020-12-20 13:02:49 -0700

    first commit of hello world

---
+++ hello/Hello.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class car
+{
+private:
+    int vin;
+    
+};
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+}
